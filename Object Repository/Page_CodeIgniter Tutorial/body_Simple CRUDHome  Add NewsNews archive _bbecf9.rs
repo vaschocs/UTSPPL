4f5608c8e2fd20ca310d77d3c7e23d852429c332
Eva Kristina <evakristina2603@gmail.com>
2020-03-23 16:53:46 +0700

    uts katalon

---
+++ Object Repository/Page_CodeIgniter Tutorial/body_Simple CRUDHome  Add NewsNews archive _bbecf9.rs
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>body_Simple CRUDHome  Add NewsNews archive _bbecf9</name>
+   <tag></tag>
+   <elementGuidId>c2a2d632-caac-43d1-abc4-51f6116f22a0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//body</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>body</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+		Simple CRUD
+		Home | Add News
+News archive
+ 
+
+    
+        Title
+        Content
+        Action
+    
+        
+            Test
+            Hello World !!
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            What is Lorem Ipsum?
+            Lorem Ipsum is simply dummy text.
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            ujian online
+            ujian online susah  banget
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            tessss 
+            menangis
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            ujian online
+            ujian online susah  banget
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+        
+            ujian online
+            ujian online susah  banget
+            
+                View | 
+                Edit | 
+                Delete
+            
+        
+
+Copyright © 2016
+
+
+/html[1]/body[1]</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//body</value>
+   </webElementXpaths>
+</WebElementEntity>
